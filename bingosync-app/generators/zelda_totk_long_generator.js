a62334f2a204e16e76cf816e66bbdbe684367266
lepelog <25211966+lepelog@users.noreply.github.com>
2023-06-27 23:41:22 +0200

    clearly the same

--- bingosync-app/generators/zelda_totk_long_generator.js
+++ bingosync-app/generators/zelda_totk_long_generator.js
@@ -1,5 +1,5 @@
 _bingoGenerator = require("./generators/generator_bases/srl_generator_v5.js");
-bingoList = require("./generators/goal_lists/zelda_botw_english.js");
+bingoList = require("./generators/goal_lists/zelda_totk_english.js");
 
 bingoGenerator = function(goals, opts) {
     opts.mode = "long";
